b9683f1aad03236623412a3ff60b150532438257
Dilnoza Tasheva <dilnozatasheva@gmail.com>
2024-10-07 12:38:34 +0200

    Created UI component Modal Window and added Close button working through the function. Adjusted some styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
+import Modal from './Components/UI/Modal/Modal.tsx';
+import { useState } from 'react';
+
 const App = () => {
+
+  const [showModal, setShowModal] = useState<boolean>(false);
+
+  const closeModalWindow = () => {
+    setShowModal(!showModal);
+  };
 
   return (
     <>
-
+      <Modal
+        show={showModal}
+        title="Some modal title"
+        closeModal={closeModalWindow}
+      >
+        <p>This is modal content</p>
+      </Modal>
+      <button className="btn btn-primary m-4" onClick={() => setShowModal(!showModal)}>Open modal window</button>
     </>
   )
 };
---
+++ src/Components/UI/Modal/Modal.tsx
@@ -0,0 +1,32 @@
+import Backdrop from '../Backdrop/Backdrop.tsx';
+import * as React from 'react';
+
+interface Props extends React.PropsWithChildren {
+  show: boolean;
+  title: string;
+  closeModal: () => void;
+}
+
+
+const Modal: React.FC<Props> = ({show, title, children, closeModal}) => {
+  return (
+      <>
+        <Backdrop show={show}/>
+        <div className="modal show" style={{display: show ? 'block' : 'none'}}>
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header d-flex justify-content-between align-items-center">
+                <h1 className="modal-title fs-5">{title}</h1>
+                  <button className="btn btn-danger" onClick={closeModal}>x</button>
+              </div>
+              <div className="p-2">
+                {children}
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+  );
+};
+
+export default Modal;
